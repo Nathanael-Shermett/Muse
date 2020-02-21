3c54c1dbb6722ed48b3b6304fb8586ba36e966c5
Nathanael Shermett <nathanael@shermett.me>
2020-02-20 20:27:25 -0500

    Fixed broken Twig 'ago' filter.

--- config/bundles.php
+++ config/bundles.php
@@ -1,17 +1,23 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+	Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                => ['all' => true],
+	Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+	Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class                 => ['all' => TRUE],
+	Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class     => ['all' => TRUE],
+	Symfony\Bundle\SecurityBundle\SecurityBundle::class                  => ['all' => TRUE],
+	Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class            => ['all' => TRUE],
+	Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => [
+		'dev'  => TRUE,
+		'test' => TRUE,
+	],
+	Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => TRUE],
+	Symfony\Bundle\MonologBundle\MonologBundle::class                    => ['all' => TRUE],
+	Symfony\Bundle\DebugBundle\DebugBundle::class                        => [
+		'dev'  => TRUE,
+		'test' => TRUE,
+	],
+	Symfony\Bundle\MakerBundle\MakerBundle::class                        => ['dev' => TRUE],
+	Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class               => ['all' => TRUE],
+	Knp\Bundle\TimeBundle\KnpTimeBundle::class                           => ['all' => TRUE],
 ];
